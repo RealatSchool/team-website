f637083c10aa0f29215f4e7ae6a7feb7308021d6
RealAwesomeness <RealAwesomeness@outlook.com>
2019-11-29 17:29:20 -0500

    very iffy node app + redis

---
+++ main.js
@@ -0,0 +1,26 @@
+const http = require('http');
+const fs = require("fs");
+const redis = require('redis');
+
+var redisClient = redis.createClient(); //creates a new client
+const hostname = '127.0.0.1';
+const port = 420;
+const template = fs.readFileSync('website/template.html', 'utf8');
+
+function respond(req,res) {
+    if (req.url === "/") {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        client.get('index', function(err, reply) {
+            var content = reply
+        });
+        res.end(template.replace("{{content}}", reply));
+    }
+}
+const server = http.createServer((req, res) => {
+    respond(req,res)
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
