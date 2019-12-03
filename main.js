6f82d3d4e7558a1a219b7c1c855d05867c6032f0
RealAwesomeness <RealAwesomeness@outlook.com>
2019-12-02 21:06:41 -0500

    the power of 404 compells you - also c++ stuff i guess

--- main.js
+++ main.js
@@ -7,14 +7,30 @@ const hostname = '127.0.0.1';
 const port = 420;
 const template = fs.readFileSync('website/template.html', 'utf8');
 
+function respond404(req,res) {
+    res.statusCode = 404;
+    res.res.setHeader('Content-Type', 'text/plaintext');
+    res.end("cease and desist immediately - this is a 404 btw")
+}
+
 function respond(req,res) {
     if (req.url === "/") {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'text/html');
-        client.get('index', function(err, reply) {
+        redisClient.get('index', function(err, reply) {
             var content = reply
         });
         res.end(template.replace("{{content}}", reply));
+    }
+    if (req.url === "/resource") {
+        var arguments = url.parse(req.url,true).query;
+        try {
+            //check to see if they're trying to access something outside current directory
+            if (arguments.file.indexOf(__dirname) > -1){
+                throw("../ detected");
+            }
+            var file = fs.readFileSync(merge('website/', arguments.file))
+        }
     }
 }
 const server = http.createServer((req, res) => {
